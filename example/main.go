bed9e5abb82a1556817284fc42b4123e7cc83747
Rodrigo Fonseca <rodrigo.fonseca@qive.com.br>
2025-06-12 10:08:55 -0300

    feat: automatic op
    Adds Op automatically when using `errors.With`. The default value comes
    from go's runtime and will be something like package.funcname.
    
    If an Op was manually added the automatic Op will be skipped.
    
    For anonymous functions a line number is also added.
    
    Both the line number and automatic op can be turned off by Global
    variables and environment variables.

--- example/main.go
+++ example/main.go
@@ -1,40 +1,85 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/arquivei/errors"
 )
 
-func doStuff() error {
-	const op = errors.Op("doStuff")
-	err := fmt.Errorf("some error")
+type name string
 
-	return errors.With(err,
-		errors.SeverityRuntime, op,
-		errors.Code("RUNTIME_ERROR"),
+func (n name) String() string {
+	return string(n)
+}
+
+type greeter[T fmt.Stringer] struct {
+	name T
+}
+
+func (g greeter[T]) sayHello() error {
+	if g.name.String() != "" {
+		fmt.Printf("Hello, %s!\n", g.name)
+		return nil
+	}
+	return errors.With(fmt.Errorf("name cannot be empty"),
+		errors.SeverityInput,
+		errors.Code("BAD_REQUEST"),
 		errors.KV("context1", "value1"),
 		errors.KV("context2", "value2"),
 	)
 }
 
-func doMoreStuff() error {
-	err := doStuff()
-	return errors.With(err,
-		errors.SeverityFatal,
-		errors.Op("doMoreStuff"),
-		errors.KV("context3", "value3"),
-	)
+func greetings(n name) error {
+	err := greeter[name]{name: n}.sayHello()
+	if err != nil {
+		// This is to show that the Op will not be added twice and context1 will be overridden
+		err = errors.With(err, errors.KV("context1", "this will be overridden"))
+
+		return errors.With(err,
+			errors.SeverityRuntime,
+			errors.Code("RUNTIME_ERROR"),
+			errors.KV("context1", "value1"),
+			errors.KV("context2", "value2"),
+		)
+	}
+	fmt.Println("Greetings sent successfully!")
+	return nil
+}
+
+func doGreetings(n name) (err error) {
+	err = greetings(n)
+	if err != nil {
+		err = errors.With(err)
+	}
+	// Wrapping in an anonymous function to simulate a call stack without caller name
+	defer func() {
+		if err != nil {
+			err = errors.With(err,
+				errors.SeverityFatal,
+				errors.KV("context3", "value3"),
+			)
+		}
+	}()
+
+	return
 }
 
 func main() {
+	flag.Parse()
+	n := ""
+	if flag.NArg() > 0 {
+		n = flag.Arg(0)
+	}
+	err := doGreetings(name(n))
+	err = errors.With(err, errors.Op("customOpExample"))
 
-	err := doMoreStuff()
-	fmt.Println(err)
+	fmt.Println("Default formatter ==>", err)
 
 	err = errors.With(err, errors.Formatter(func(err error) string {
 		return fmt.Sprintf("formatted error: %s", errors.GetRootError(err))
 	}))
 
-	fmt.Println(err)
+	fmt.Println("Custom formatter ==>", err)
+
 }
